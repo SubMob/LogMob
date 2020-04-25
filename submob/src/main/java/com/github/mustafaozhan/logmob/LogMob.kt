d0016c4ba4b9abaee458d8e3a54fc53da180e0e3
Mustafa Ozhan <mustafa.ozhan@massive.co>
2020-04-25 19:10:00 +0200

    adding logging only with message

--- submob/src/main/java/com/github/mustafaozhan/logmob/LogMob.kt
+++ submob/src/main/java/com/github/mustafaozhan/logmob/LogMob.kt
@@ -29,19 +29,25 @@ fun initLogMob(context: Context, enableCrashlytics: Boolean = false) {
 }
 
 fun logVerbose(e: Throwable) = Timber.v(e)
+fun logVerbose(message: String) = Timber.v(message)
 fun logVerbose(e: Throwable, message: String) = Timber.v(e, message)
 
 fun logDebug(e: Throwable) = Timber.d(e)
+fun logDebug(message: String) = Timber.d(message)
 fun logDebug(e: Throwable, message: String) = Timber.d(e, message)
 
 fun logInfo(e: Throwable) = Timber.i(e)
+fun logInfo(message: String) = Timber.i(message)
 fun logInfo(e: Throwable, message: String) = Timber.i(e, message)
 
 fun logWarning(e: Throwable) = Timber.w(e)
+fun logWarning(message: String) = Timber.w(message)
 fun logWarning(e: Throwable, message: String) = Timber.w(e, message)
 
 fun logError(e: Throwable) = Timber.e(e)
+fun logError(message: String) = Timber.e(message)
 fun logError(e: Throwable, message: String) = Timber.e(e, message)
 
 fun logAssert(e: Throwable) = Timber.wtf(e)
+fun logAssert(message: String) = Timber.wtf(message)
 fun logAssert(e: Throwable, message: String) = Timber.wtf(e, message)
