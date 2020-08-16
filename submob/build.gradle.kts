68293c72d23dcbb3d354014927a37b352ceed769
Mustafa Ozhan <mustafa.ozhan@massive.co>
2020-08-16 14:57:19 +0200

    moving to Kotlin DSL and buildSrc

---
+++ submob/build.gradle.kts
@@ -0,0 +1,32 @@
+/*
+ Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
+ */
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    with(ProjectSettings) {
+        compileSdkVersion(projectCompileSdkVersion)
+
+        defaultConfig {
+            minSdkVersion(projectMinSdkVersion)
+            targetSdkVersion(projectTargetSdkVersion)
+
+            versionCode = getVersionCode(project)
+            versionName = getVersionName(project)
+        }
+    }
+}
+
+dependencies {
+    with(Dependencies) {
+        implementation(kotlin)
+        implementation(firebaseCrashlytics)
+        implementation(firebaseCore)
+        implementation(timber)
+        implementation(anrWatchDog)
+    }
+}
