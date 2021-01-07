78c2a880629403d0771aab319c1bb67af0ce91cf
Mustafa Ozhan <mustafa.ozhan@massive.co>
2021-01-07 09:59:37 +0100

    android plugin update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
+ * Copyright (c) 2021 Mustafa Ozhan. All rights reserved.
  */
 
 object Versions {
     const val kotlin = "1.4.30-M1"
-    const val androidPlugin = "7.0.0-alpha03"
+    const val androidPlugin = "7.0.0-alpha04"
     const val firebaseCore = "18.0.0"
     const val firebaseCrashlytics = "17.3.0"
     const val anrWatchDog = "1.4.0"
