c3f9f0ba185cb1faf485a64c0b863be25f0da808
Mustafa Ozhan <mustafa.ozhan@massive.co>
2021-03-22 23:25:47 +0100

    library updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@
 
 object Versions {
     const val kotlin = "1.4.31"
-    const val androidPlugin = "7.0.0-alpha10"
+    const val androidPlugin = "7.0.0-alpha11"
     const val firebaseCore = "18.0.2"
     const val firebaseCrashlytics = "17.4.0"
     const val anrWatchDog = "1.4.0"
