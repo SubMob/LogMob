c265fbd4fd61b96b380f270a369c694a97e65ac2
Mustafa Ozhan <mustafa.ozhan@massive.co>
2021-05-09 15:42:22 +0300

    kotlin and plugin updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,8 +3,8 @@
  */
 
 object Versions {
-    const val kotlin = "1.5.0-RC"
-    const val androidPlugin = "7.0.0-alpha14"
+    const val kotlin = "1.5.0"
+    const val androidPlugin = "7.0.0-alpha15"
     const val firebaseCore = "18.0.3"
     const val firebaseCrashlytics = "17.4.1"
     const val anrWatchDog = "1.4.0"
