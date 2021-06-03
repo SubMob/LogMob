9a4b77d28450ee7cc593a7eef8ee7c02ef8cab5b
Mustafa Ozhan <mustafa.ozhan@massive.co>
2021-06-03 16:30:03 +0300

    updating kotlin version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@
  */
 
 object Versions {
-    const val kotlin = "1.5.0"
+    const val kotlin = "1.5.10"
     const val androidPlugin = "7.1.0-alpha01"
     const val firebaseCore = "19.0.0"
     const val firebaseCrashlytics = "18.0.0"
