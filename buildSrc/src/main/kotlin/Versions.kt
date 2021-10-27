a58678401a8d57be8f8cff4e8c255025f6edfb17
Mustafa Ozhan <mustafa.ozhan@wetter.com>
2021-10-27 22:42:36 +0200

    gradle and android gradle plugin updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@
 
 object Versions {
     const val KOTLIN = "1.5.31"
-    const val ANDROID_GRADLE_PLUGIN = "7.2.0-alpha02"
+    const val ANDROID_GRADLE_PLUGIN = "7.2.0-alpha03"
     const val FIREBASE_CORE = "19.0.2"
     const val FIREBASE_CRASHLYTICS = "18.2.3"
     const val ANR_WATCH_DOG = "1.4.0"
