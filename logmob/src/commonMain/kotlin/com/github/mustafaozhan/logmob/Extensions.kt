316eaf1502fe587370081534bbe21291a68338c2
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-02-17 19:39:21 +0100

    [Oztechan/CCC#461] remove unused method

--- logmob/src/commonMain/kotlin/com/github/mustafaozhan/logmob/Extensions.kt
+++ logmob/src/commonMain/kotlin/com/github/mustafaozhan/logmob/Extensions.kt
@@ -3,5 +3,3 @@ package com.github.mustafaozhan.logmob
 import co.touchlab.kermit.Logger
 
 fun Logger.e(throwable: Throwable) = e(throwable) { throwable.message.toString() }
-
-fun Logger.w(throwable: Throwable) = w(throwable) { throwable.message.toString() }
