3becd489b93afb24c2de0d8cc7c221455106e5f0
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-03-26 12:28:09 +0100

    [Oztechan/CCC#565] Library updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@
 object Versions {
     const val KOTLIN = "1.6.10"
     const val ANDROID_GRADLE_PLUGIN = "7.1.2"
-    const val FIREBASE_CRASHLYTICS = "18.2.8"
+    const val FIREBASE_CRASHLYTICS = "18.2.9"
     const val ANR_WATCH_DOG = "1.4.0"
     const val KERMIT = "1.0.3"
 }
