3b49639170baca8c9d3abb1f738561abf52539ab
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-09-09 20:46:51 +0200

    [#19] Update Libraries (#20)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,10 +4,10 @@
 
 object Versions {
     const val KOTLIN = "1.7.10"
-    const val ANDROID_GRADLE_PLUGIN = "7.2.1"
-    const val FIREBASE_CRASHLYTICS = "18.2.11"
+    const val ANDROID_GRADLE_PLUGIN = "7.2.2"
+    const val FIREBASE_CRASHLYTICS = "18.2.13"
     const val ANR_WATCH_DOG = "1.4.0"
     const val KERMIT = "1.1.3"
     const val DEPENDENCY_UPDATES = "0.42.0"
-    const val BUILD_HEALTH = "1.9.0"
+    const val BUILD_HEALTH = "1.13.1"
 }
