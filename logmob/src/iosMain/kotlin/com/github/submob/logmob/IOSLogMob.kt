725d47542304604214e4075dd2dc188633ee199b
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-10-09 16:12:24 +0300

    [Oztechan/CCC#1182] Reorganise logging and add kermit transitive

--- logmob/src/iosMain/kotlin/com/github/submob/logmob/IOSLogMob.kt
+++ logmob/src/iosMain/kotlin/com/github/submob/logmob/IOSLogMob.kt
@@ -6,9 +6,8 @@ import co.touchlab.kermit.crashlytics.CrashlyticsLogWriter
 import co.touchlab.kermit.crashlytics.setupCrashlyticsExceptionHook
 
 @Suppress("OPT_IN_USAGE", "unused")
-fun initIOSLogger(
-    isCrashlyticsEnabled: Boolean = false
-) = initLogger().also {
+fun initIOSLogger(isCrashlyticsEnabled: Boolean = false) {
+    initLogger()
     // Setting CrashlyticsLogWriter doesn't work it has to be added later
     if (isCrashlyticsEnabled) {
         Logger.addLogWriter(
