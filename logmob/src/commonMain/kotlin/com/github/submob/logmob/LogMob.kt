3b03254117150afb8e9af32bad3f57b0663da2b3
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-10-10 21:25:25 +0300

    [Oztechan/CCC#1182] Reorganise logging and add kermit transitive

--- logmob/src/commonMain/kotlin/com/github/submob/logmob/LogMob.kt
+++ logmob/src/commonMain/kotlin/com/github/submob/logmob/LogMob.kt
@@ -1,12 +1,15 @@
 package com.github.submob.logmob
 
+import co.touchlab.kermit.LogWriter
 import co.touchlab.kermit.Logger
 
 @Suppress("unused")
-fun initLogger() = Logger.setLogWriters(
-    LogMobWriter()
+fun initLogger(logWriters: List<LogWriter> = emptyList()) = Logger.setLogWriters(
+    logWriters + LogMobWriter()
 ).also {
     Logger.v { "Logger initialized" }
+}.let {
+    Logger
 }
 
 fun initTestLogger() = Logger.setLogWriters(
--- logmob/src/iosMain/kotlin/com/github/submob/logmob/IOSLogMob.kt
+++ logmob/src/iosMain/kotlin/com/github/submob/logmob/IOSLogMob.kt
@@ -1,27 +1,24 @@
 package com.github.submob.logmob
 
-import co.touchlab.kermit.Logger
 import co.touchlab.kermit.Severity
 import co.touchlab.kermit.crashlytics.CrashlyticsLogWriter
 import co.touchlab.kermit.crashlytics.setupCrashlyticsExceptionHook
 
-@Suppress("unused")
+@Suppress("unused", "OPT_IN_USAGE")
 fun initIOSLogger(isCrashlyticsEnabled: Boolean = false) {
-    initLogger()
-    // Setting CrashlyticsLogWriter doesn't work it has to be added later
     if (isCrashlyticsEnabled) {
-        initCrashlytics()
+        initLogger(
+            listOf(
+                CrashlyticsLogWriter(
+                    minSeverity = Severity.Verbose,
+                    minCrashSeverity = Severity.Warn,
+                    printTag = true
+                )
+            )
+        ).also {
+            setupCrashlyticsExceptionHook(it)
+        }
+    } else {
+        initLogger()
     }
-}
-
-@Suppress("OPT_IN_USAGE", "unused")
-fun initCrashlytics() {
-    Logger.addLogWriter(
-        CrashlyticsLogWriter(
-            minSeverity = Severity.Verbose,
-            minCrashSeverity = Severity.Warn,
-            printTag = true
-        )
-    )
-    setupCrashlyticsExceptionHook(Logger)
 }
