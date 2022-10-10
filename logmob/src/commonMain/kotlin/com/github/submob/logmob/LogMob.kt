2f5357f479618895436d75e3a10b84690530f087
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-10-10 23:13:54 +0300

    [Oztechan/CCC#1182] Reorganise logging and add kermit transitive

--- logmob/src/commonMain/kotlin/com/github/submob/logmob/LogMob.kt
+++ logmob/src/commonMain/kotlin/com/github/submob/logmob/LogMob.kt
@@ -1,15 +1,12 @@
 package com.github.submob.logmob
 
-import co.touchlab.kermit.LogWriter
 import co.touchlab.kermit.Logger
 
 @Suppress("unused")
-fun initLogger(logWriters: List<LogWriter> = emptyList()) = Logger.setLogWriters(
-    logWriters + LogMobWriter()
+fun initLogger() = Logger.setLogWriters(
+    LogMobWriter()
 ).also {
     Logger.v { "Logger initialized" }
-}.let {
-    Logger
 }
 
 fun initTestLogger() = Logger.setLogWriters(
--- logmob/src/iosMain/kotlin/com/github/submob/logmob/IOSLogMob.kt
+++ logmob/src/iosMain/kotlin/com/github/submob/logmob/IOSLogMob.kt
@@ -1,24 +1,29 @@
 package com.github.submob.logmob
 
+import co.touchlab.kermit.Logger
 import co.touchlab.kermit.Severity
 import co.touchlab.kermit.crashlytics.CrashlyticsLogWriter
 import co.touchlab.kermit.crashlytics.setupCrashlyticsExceptionHook
 
-@Suppress("unused", "OPT_IN_USAGE")
-fun initIOSLogger(isCrashlyticsEnabled: Boolean = false) {
+@Suppress("unused")
+fun initIOSLogger(isCrashlyticsEnabled: Boolean = false): Logger {
+    initLogger()
+    // Setting CrashlyticsLogWriter doesn't work it has to be added later
     if (isCrashlyticsEnabled) {
-        initLogger(
-            listOf(
-                CrashlyticsLogWriter(
-                    minSeverity = Severity.Verbose,
-                    minCrashSeverity = Severity.Warn,
-                    printTag = true
-                )
-            )
-        ).also {
-            setupCrashlyticsExceptionHook(it)
-        }
-    } else {
-        initLogger()
+        initCrashlytics()
     }
+
+    return Logger
+}
+
+@Suppress("OPT_IN_USAGE", "unused")
+fun initCrashlytics() {
+    Logger.addLogWriter(
+        CrashlyticsLogWriter(
+            minSeverity = Severity.Verbose,
+            minCrashSeverity = Severity.Warn,
+            printTag = true
+        )
+    )
+    setupCrashlyticsExceptionHook(Logger)
 }
