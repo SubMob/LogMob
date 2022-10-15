cf7e687cd9535d6b5d28a65e3ed03f17bf661388
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-10-15 13:36:12 +0300

    [#1214] Update Dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,8 +4,8 @@
 
 object Versions {
     const val KOTLIN = "1.7.20"
-    const val ANDROID_GRADLE_PLUGIN = "7.3.0"
-    const val FIREBASE_CRASHLYTICS = "18.2.13"
+    const val ANDROID_GRADLE_PLUGIN = "7.3.1"
+    const val FIREBASE_CRASHLYTICS = "18.3.0"
     const val ANR_WATCH_DOG = "1.4.0"
     const val KERMIT = "1.1.3"
     const val DEPENDENCY_UPDATES = "0.42.0"
