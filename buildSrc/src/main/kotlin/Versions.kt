e7fcb2e26e03a593a76eba72133546b049f2c2af
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-10-22 00:20:48 +0300

    [Oztechan/CCC#1244] Update Crashlytics

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@
 object Versions {
     const val KOTLIN = "1.7.20"
     const val ANDROID_GRADLE_PLUGIN = "7.3.1"
-    const val FIREBASE_CRASHLYTICS = "18.3.0"
+    const val FIREBASE_CRASHLYTICS = "18.3.1"
     const val ANR_WATCH_DOG = "1.4.0"
     const val KERMIT = "1.1.3"
     const val DEPENDENCY_UPDATES = "0.42.0"
