69016f55814825063e6ee2f84a8d14dceb938dfc
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-09-26 10:11:24 +0200

    [SubMob/LogMob#80] Use crashkios setCrashlyticsUnhandledExceptionHook in initialisation of LogMob

--- logmob/src/iosMain/kotlin/com/github/submob/logmob/Crashlytics.ios.kt
+++ logmob/src/iosMain/kotlin/com/github/submob/logmob/Crashlytics.ios.kt
@@ -1,17 +1,16 @@
 package com.github.submob.logmob
 
+import co.touchlab.crashkios.crashlytics.setCrashlyticsUnhandledExceptionHook
 import co.touchlab.kermit.Logger
 import co.touchlab.kermit.Severity
 import co.touchlab.kermit.crashlytics.CrashlyticsLogWriter
-import co.touchlab.kermit.crashlytics.setCrashlyticsUnhandledExceptionHook
 
 @Suppress("OPT_IN_USAGE", "unused")
 fun initCrashlytics() {
     Logger.addLogWriter(
         CrashlyticsLogWriter(
             minSeverity = Severity.Verbose,
-            minCrashSeverity = Severity.Warn,
-            printTag = true
+            minCrashSeverity = Severity.Warn
         )
     )
     setCrashlyticsUnhandledExceptionHook()
