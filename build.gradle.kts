45526c883ff4bca0f66c072a71572720425e334d
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-11-02 21:48:43 +0100

    [SubMob/LogMob#27] Dependency update dependency org.jetbrains.kotlin.multiplatform to v1.9.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,6 +86,7 @@ allprojects {
 
     tasks.withType<KotlinCompile> {
         kotlinOptions {
+            // todo remove when not needed anymore
             freeCompilerArgs += "-Xexpect-actual-classes"
             allWarningsAsErrors = true
         }
