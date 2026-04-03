f2b47d1ffb805a85aea80b0a3beda2a5d63a483c
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2026-04-03 22:45:21 +0200

    [Oztechan/CCC#4626] Make LogMob variant independent

--- logmob/src/androidMain/kotlin/com/github/submob/logmob/BaseLogMobWriter.kt
+++ logmob/src/androidMain/kotlin/com/github/submob/logmob/BaseLogMobWriter.kt
@@ -13,30 +13,27 @@ actual abstract class BaseLogMobWriter : LogWriter() {
     private val logger = platformLogWriter()
 
     actual override fun log(severity: Severity, message: String, tag: String, throwable: Throwable?) {
-        if (BuildConfig.DEBUG) {
-            logger.log(
-                severity = severity,
-                tag = tag,
-                throwable = throwable,
-                message = "@${Thread.currentThread().name}: $message",
-            )
-        } else {
-            FirebaseCrashlytics.getInstance().log(
-                "${severity.name} @${Thread.currentThread().name} $message"
-            )
+        logger.log(
+            severity = severity,
+            tag = tag,
+            throwable = throwable,
+            message = "@${Thread.currentThread().name}: $message",
+        )
+        FirebaseCrashlytics.getInstance().log(
+            "${severity.name} @${Thread.currentThread().name} $message"
+        )
 
-            if (severity == Severity.Error) {
-                FirebaseCrashlytics.getInstance().apply {
-                    setCustomKey(CRASHLYTICS_KEY_PRIORITY, severity.name)
-                    setCustomKey(CRASHLYTICS_KEY_TAG, tag)
-                    setCustomKey(CRASHLYTICS_KEY_MESSAGE, message)
-                    recordException(
-                        throwable ?: Exception(message).also {
-                            // To ensure that the Exceptions are not merged into single instance in long term
-                            recordException(Exception("No throwable provided for error message: $message"))
-                        }
-                    )
-                }
+        if (severity == Severity.Error) {
+            FirebaseCrashlytics.getInstance().apply {
+                setCustomKey(CRASHLYTICS_KEY_PRIORITY, severity.name)
+                setCustomKey(CRASHLYTICS_KEY_TAG, tag)
+                setCustomKey(CRASHLYTICS_KEY_MESSAGE, message)
+                recordException(
+                    throwable ?: Exception(message).also {
+                        // To ensure that the Exceptions are not merged into single instance in long term
+                        recordException(Exception("No throwable provided for error message: $message"))
+                    }
+                )
             }
         }
     }
